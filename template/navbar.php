eaa83ed8b15a828c0ea8e017e71a445cab94865c
Ryan <behaguer@gmail.com>
2022-10-23 19:38:31 +1000

    Responsive styling fixes

--- template/navbar.php
+++ template/navbar.php
@@ -3,8 +3,8 @@
           <a href="" class="uk-icon-link" uk-icon="menu" uk-toggle="target: #offcanvas-usage"></a>
           
         </div>
-        <img src="assets/images/logo.png" alt="" srcset="">
-      <div class="uk-navbar-right">
+        <img class="logo" src="assets/images/logo.png" alt="" srcset="">
+      <div class="uk-navbar-right uk-visible@m">
         <div class="uk-flex uk-flex-column">
         <h3 style="margin:0px 10px 0px 0px;">Backend Search Example</h3>
           <em>Prepared by: Ryan Behague</em>
